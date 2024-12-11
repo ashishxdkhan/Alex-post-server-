3e37c5c51e0359505c1f38496c0c6f226daa3e58
ashishxdkhan <ashishxd4@gmail.com>
2024-12-11 22:02:31 +0545

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, redirect, url_for, render_template_string
+import requests
+import time
+
+app = Flask(__name__)
+
+headers = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
+    'referer': 'www.google.com'
+}
+
+@app.route('/')
+def index():
+    return render_template_string('''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ALEX POST</title>
+    <style>
+        body {
+            background-image: url(https://i.ibb.co/MV2yzNB/687fe2de54ad8c41756319b59e7d0578.jpg);
